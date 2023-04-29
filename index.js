64596c2e6dfb25149608398ef6ef5e1716d0d7df
Abu Abdullah <abdullah72308@gmail.com>
2023-04-29 22:28:48 +0600

    vercel added

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.get('/categories', (req, res) =>{
 
 app.get('/news', (req, res)=>{
     res.send(news)
+});
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(newsId => newsId._id === id)
+    res.send(selectedNews)
+});
+
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id === '0'){
+        res.send(news)
+    } else{
+
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, ()=>{
